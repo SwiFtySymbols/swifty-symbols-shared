1b41a96d81295ebd902349265345c3cf516b67bb
Michael Redig <mredig@gmail.com>
2020-07-04 16:29:35 -0500

    (feat) added result object

--- Sources/SwiFtySymbolsShared/SFSymbolModelObjects.swift
+++ Sources/SwiFtySymbolsShared/SFSymbolModelObjects.swift
@@ -12,6 +12,20 @@ public struct SFSymbolListObject: Codable, Hashable {
 	}
 }
 
+public struct SFSymbolResultObject: Codable, Hashable {
+	public let id: UUID
+	public let value: String
+	public let availability: SFVersionAvailability
+	public let resultScore: Double
+
+	public init(id: UUID, value: String, availability: SFVersionAvailability, resultScore: Double) {
+		self.id = id
+		self.value = value
+		self.availability = availability
+		self.resultScore = resultScore
+	}
+}
+
 public struct SFSymbolGetObject: Codable {
 	public let id: UUID
 	public let value: String
