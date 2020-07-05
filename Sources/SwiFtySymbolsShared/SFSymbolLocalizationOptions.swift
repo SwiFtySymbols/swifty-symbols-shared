cdd3f5a191a3994ca28aaf0be7b0c287ce7fc593
Michael Redig <mredig@gmail.com>
2020-07-05 04:14:20 -0500

    (feat) added localization option enum

---
+++ Sources/SwiFtySymbolsShared/SFSymbolLocalizationOptions.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum SFSymbolLocalizationOptions: String, Codable, Hashable {
+	case rightToLeft = "rtl"
+	case arabic = "ar"
+	case devanagari = "hi"
+	case hebrew = "he"
+}
