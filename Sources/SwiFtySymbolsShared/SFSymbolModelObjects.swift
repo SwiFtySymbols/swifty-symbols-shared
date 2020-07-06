0ed4e597eeeb9bd98f5415dafd859fd5d29c8aa9
Michael Redig <mredig@gmail.com>
2020-07-06 17:10:02 -0500

    (feat) added localization and deprecation for symbol models

--- Sources/SwiFtySymbolsShared/SFSymbolModelObjects.swift
+++ Sources/SwiFtySymbolsShared/SFSymbolModelObjects.swift
@@ -3,11 +3,15 @@ import Foundation
 public struct SFSymbolListObject: Codable, Hashable {
 	public let id: UUID
 	public let value: String
+	public let localizations: [SFSymbolLocalizationOptions]?
+	public let deprecatedNames: [String]?
 	public let availability: SFVersionAvailability
 
-	public init(id: UUID, value: String, availability: SFVersionAvailability) {
+	public init(id: UUID, value: String, localizations: [SFSymbolLocalizationOptions], deprecatedNames: [String]?, availability: SFVersionAvailability) {
 		self.id = id
 		self.value = value
+		self.localizations = localizations
+		self.deprecatedNames = deprecatedNames
 		self.availability = availability
 	}
 }
@@ -16,12 +20,16 @@ public struct SFSymbolResultObject: Codable, Hashable {
 	public let id: UUID
 	public let value: String
 	public let availability: SFVersionAvailability
+	public let localizations: [SFSymbolLocalizationOptions]
+	public let deprecatedNames: [String]
 	public let resultScore: Double
 
-	public init(id: UUID, value: String, availability: SFVersionAvailability, resultScore: Double) {
+	public init(id: UUID, value: String, availability: SFVersionAvailability, localizations: [SFSymbolLocalizationOptions], deprecatedNames: [String], resultScore: Double) {
 		self.id = id
 		self.value = value
 		self.availability = availability
+		self.localizations = localizations
+		self.deprecatedNames = deprecatedNames
 		self.resultScore = resultScore
 	}
 }
@@ -30,12 +38,16 @@ public struct SFSymbolGetObject: Codable {
 	public let id: UUID
 	public let value: String
 	public let availability: SFVersionAvailability
+	public let localizations: [SFSymbolLocalizationOptions]
+	public let deprecatedNames: [String]
 	public var tags: [SFSymbolTagListObject]?
 
-	public init(id: UUID, value: String, availability: SFVersionAvailability, tags: [SFSymbolTagListObject]? = nil) {
+	public init(id: UUID, value: String, availability: SFVersionAvailability, localizations: [SFSymbolLocalizationOptions], deprecatedNames: [String], tags: [SFSymbolTagListObject]? = nil) {
 		self.id = id
 		self.value = value
 		self.availability = availability
+		self.localizations = localizations
+		self.deprecatedNames = deprecatedNames
 		self.tags = tags
 	}
 }
